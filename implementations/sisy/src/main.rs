c5484cebd21c50dcc76ad6108e8770dc4eaf2e07
Sam LaGrave <srlagrave@gmail.com>
2024-04-24 18:13:26 -0500

    Starting rust impl

--- implementations/sisy/src/main.rs
+++ implementations/sisy/src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::HashMap;
+use std::fs;
+
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "syscript.pest"]
+pub struct SyParser;
+
 fn main() {
-    println!("Hello, world!");
+    let unparsed_file = fs::read_to_string("test.sy").expect("Cannot read file");
 }
