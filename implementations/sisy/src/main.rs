8465fffbbab0c46e25006d2088ba9b5fde75637d
Sam LaGrave <srlagrave@gmail.com>
2024-04-29 10:32:37 -0500

    Actually parsing a file now

--- implementations/sisy/src/main.rs
+++ implementations/sisy/src/main.rs
@@ -1,5 +1,6 @@
 // use std::collections::HashMap;
 use std::fs;
+use std::vec::Vec;
 
 use pest::Parser;
 use pest_derive::Parser;
@@ -8,6 +9,17 @@ use pest_derive::Parser;
 #[grammar = "syscript.pest"]
 pub struct SyParser;
 
+#[derive(Debug)]
+pub enum SyLineType {
+    COMMENT,
+    COMMAND,
+}
+
+#[derive(Debug)]
+pub struct SyLine {
+    _line_type: SyLineType,  // TODO: Remove underscore
+}
+
 fn main() {
     let unparsed_file = fs::read_to_string("./test.sy").expect("Cannot read file");
     
@@ -15,7 +27,21 @@ fn main() {
         .expect("Unsuccessful parse")
         .next().unwrap();
 
+    let mut lines: Vec<SyLine> = Vec::new();
+
     for line in file.into_inner() {
-        println!("{:?}", line);
+        match line.as_rule() {
+            Rule::comment => {
+                let new_line = SyLine { _line_type: SyLineType::COMMENT };
+                lines.push(new_line);
+            }
+            Rule::command => {
+                let new_line = SyLine { _line_type: SyLineType::COMMAND };
+                lines.push(new_line);
+            }
+            _ => {}
+        }
     }
+
+    println!("{:#?}", lines);
 }
