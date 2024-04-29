73a8b159a624fdf66c96ff2ab75b856999c0cf02
Sam LaGrave <srlagrave@gmail.com>
2024-04-29 15:08:18 -0500

    More useful parsing.

--- implementations/sisy/src/main.rs
+++ implementations/sisy/src/main.rs
@@ -8,17 +8,31 @@ use pest_derive::Parser;
 #[grammar = "syscript.pest"]
 pub struct SyParser;
 
-#[derive(Debug)]
-pub enum SyLineType {
-    COMMENT,
-    COMMAND,
-    LEAF,
+#[derive(Debug, Clone)]
+pub struct SyOperand {
+    _noop: bool,
+    _int: Option<i32>,
+    ident: Option<String>,
 }
 
 #[derive(Debug)]
-pub struct SyLine {
-    _line_type: SyLineType,  // TODO: Remove underscore
-    _span: String,  // TODO: Remove underscore
+pub struct SyCommandSy {
+    idx: u32,
+    arg0: SyOperand,
+    arg1: SyOperand,
+    arg2: SyOperand,
+    arg3: SyOperand,
+}
+
+#[derive(Debug)]
+pub struct SyCommandLeaf {
+    idx: u32,
+    ident: String,
+}
+
+#[derive(Debug)]
+pub struct SyComment {
+    span: String,
 }
 
 fn main() {
@@ -28,25 +42,57 @@ fn main() {
         .expect("Unsuccessful parse")
         .next().unwrap();
 
-    let mut lines: Vec<SyLine> = Vec::new();
+    let mut comments: Vec<SyComment> = Vec::new();
+    let mut sys: Vec<SyCommandSy> = Vec::new();
+    let mut leafs: Vec<SyCommandLeaf> = Vec::new();
 
+    let mut idx = 0;
     for line in file.into_inner() {
         match line.as_rule() {
             Rule::comment => {
-                let new_line = SyLine { _line_type: SyLineType::COMMENT, _span: line.as_str().to_string() };
-                lines.push(new_line);
+                comments.push( SyComment{ span: line.as_str().to_string() } );
             }
             Rule::command_sy => {
-                let new_line = SyLine { _line_type: SyLineType::COMMAND, _span: line.as_str().to_string() };
-                lines.push(new_line);
+                let inner = line.into_inner();
+                let mut args: Vec<SyOperand> = Vec::new();
+                for arg in inner {
+                    match arg.as_rule() {
+                        Rule::operand_noop => {
+                            args.push(SyOperand { _noop: true, _int: None, ident: None, });
+                        }
+                        Rule::operand_integer => {
+                            let s = arg.as_str().to_string();
+                            let i = s.parse::<i32>().unwrap();
+                            args.push(SyOperand { _noop: false, _int: Some(i), ident: None, });
+                        }
+                        Rule::operand_identifier => {
+                            let s = arg.as_str().to_string();
+                            args.push(SyOperand { _noop: false, _int: None, ident: Some(s), });
+                        }
+                        _ => {}
+                    }
+                }
+                sys.push( SyCommandSy{
+                    idx,
+                    arg0: args.get(0).unwrap().clone(),
+                    arg1: args.get(1).unwrap().clone(),
+                    arg2: args.get(2).unwrap().clone(),
+                    arg3: args.get(3).unwrap().clone(),
+                } );
+                idx += 1;
             }
             Rule::command_leaf => {
-                let new_line = SyLine { _line_type: SyLineType::LEAF, _span: line.as_str().to_string() };
-                lines.push(new_line);
+                let ident = line.into_inner().next().unwrap().as_str().to_string();
+                leafs.push( SyCommandLeaf{ idx, ident } );
+            }
+            Rule::EOI => {
+                println!("EOI is idx = {}.", idx);
             }
             _ => {}
         }
     }
 
-    println!("{:#?}", lines);
+    println!("{:#?}", comments);
+    println!("{:#?}", sys);
+    println!("{:#?}", leafs);
 }
